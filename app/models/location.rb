a90438e62202b2d9c0c2eba60b3b1f0c02f44133
RollandMichael7 <rollandmichael7@gmail.com>
2023-02-09 01:09:23 -0500

    Display badge for newly updated objects

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,7 @@
 class Location < ApplicationRecord
 	has_many :images
 	has_many :subjects, -> { distinct }, through: :images
+	has_many :imports, -> { distinct }, through: :images
 
 	validates :name, uniqueness: true
 end
