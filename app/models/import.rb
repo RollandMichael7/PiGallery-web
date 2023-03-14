1994db6cd917ec462507c90a46de6428bccdfcf7
RollandMichael7 <rollandmichael7@gmail.com>
2023-03-13 21:25:06 -0400

    Standardize new badge, add to image cards

--- app/models/import.rb
+++ app/models/import.rb
@@ -2,4 +2,8 @@ class Import < ApplicationRecord
 	has_many :images
 	has_many :locations, -> { distinct }, through: :images
 	has_many :subjects, -> { distinct }, through: :images
+
+	def new?
+		created_at >= (Time.zone.now - CONFIG[:new_period_days].days)
+	end
 end
