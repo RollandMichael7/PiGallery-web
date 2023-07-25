07e05d928a88c75ac8ad3b88147ef47880feef9c
RollandMichael7 <rollandmichael7@gmail.com>
2023-07-24 20:09:43 -0400

    Use separate API keys for BE and FE
    backend API cant be used with referrer restrictions

--- config/initializers/google_maps.rb
+++ config/initializers/google_maps.rb
@@ -1,4 +1,4 @@
 Google::Maps.configure do |config|
   config.authentication_mode = Google::Maps::Configuration::API_KEY
-  config.api_key = ENV["PIGALLERY_GOOGLE_MAPS_KEY"]
+  config.api_key = ENV["PIGALLERY_GOOGLE_MAPS_BE_KEY"]
 end
