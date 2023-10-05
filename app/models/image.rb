56d51a8074c4c92981b1d8683df7581d835fc5c8
RollandMichael7 <rollandmichael7@gmail.com>
2023-10-04 22:41:55 -0400

    Add name details to image cards

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,15 @@
 class Image < ApplicationRecord
+	NAME_DETAIL_JUVENILE = "j.".freeze
+	NAME_DETAIL_FEMALE = "f.".freeze
+	NAME_DETAIL_MALE = "m.".freeze
+	NAME_DETAIL_CAPTIVE = "captive".freeze
+
+	FULL_NAME_DETAILS = {
+		NAME_DETAIL_JUVENILE => "Juvenile",
+		NAME_DETAIL_MALE => "Male",
+		NAME_DETAIL_FEMALE => "Female",
+	}
+
 	belongs_to :subject
 	belongs_to :location
 	belongs_to :month
@@ -16,5 +27,14 @@ class Image < ApplicationRecord
 
 	def image_path
 		File.join(Rails.root, 'app', 'assets', 'images', file_name)
+	end
+
+	def full_name_detail
+		name_details = self.name_detail&.split(",")&.map do |detail|
+			detail = detail.strip
+			detail = FULL_NAME_DETAILS[detail.downcase] || detail
+		end
+
+		name_details&.join(", ")
 	end
 end
