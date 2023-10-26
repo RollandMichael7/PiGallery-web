de85ded2693f4bf53757d5578d77fbe776fc69ce
RollandMichael7 <rollandmichael7@gmail.com>
2023-10-26 10:36:21 -0400

    Sort import images in reverse chronological order

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -10,7 +10,7 @@ class ImportsController < ApplicationController
 		@import = Import.find(params[:id])
 
 		page_size = browser.device.mobile? ? 10 : 5
-		@images = @import.images.order(date_taken: :desc).page(params[:page]).per(page_size)
+		@images = @import.images.order(:date_taken).page(params[:page]).per(page_size)
 	end
 
 	private
